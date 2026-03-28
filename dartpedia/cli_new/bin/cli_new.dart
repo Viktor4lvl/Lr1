d350fa5cee982ae137252c5370aad4a896855c56
Viktor4lvl/lr1.git <vitya.podolskiy.01@mail.ru>
2026-03-28 08:57:24 +0300

    Ошибку calculate ( неопределение функции) я смог решить тем, что создал новый  cli_new.dart и увидел, что функция caculate имеет значения расчета тестируемой функции калькулятора и по заданию в cli.dart удаляются строки, но остаётся эта зависимость, поэтому с cli_new скопировал int calculate и вставил его в cli.dart

---
+++ dartpedia/cli_new/bin/cli_new.dart
@@ -0,0 +1,5 @@
+import 'package:cli_new/cli_new.dart' as cli_new;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cli_new.calculate()}!');
+}
---
+++ dartpedia/cli_new/test/cli_new_test.dart
@@ -0,0 +1,8 @@
+import 'package:cli_new/cli_new.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
